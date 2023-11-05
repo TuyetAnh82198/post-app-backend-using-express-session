659feb8a2c6c038ce98a3bb029d92a0ba52afdb6
Tuyết Anh <tuyetanh82198@gmail.com>
2023-11-05 21:02:37 +0700

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,7 +3,8 @@ module.exports = {
   init: (httpServer) => {
     io = require("socket.io")(httpServer, {
       cors: {
-        origin: process.env.CLIENT_APP,
+        origin: [process.env.CLIENT_APP,process.env.CLIENT_APP2],
+        default: process.env.CLIENT_APP,
         allowedHeaders: [
           "Access-Control-Allow-Origin",
           "Access-Control-Allow-Credentials",
